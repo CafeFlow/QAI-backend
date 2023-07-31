b37eb1d5a667ca781d8a80ccdd2f7f71919031d9
Ahnheechan <ahc70032@gmail.com>
2023-07-31 23:18:42 +0900

    답변 수정,삭제 구현 맟 도커 이미지 빌드를 위한 dockerfile 생성

---
+++ likelion/src/main/java/com/power/likelion/common/exception/AuthorMismatchException.java
@@ -0,0 +1,7 @@
+package com.power.likelion.common.exception;
+
+public class AuthorMismatchException extends Exception{
+    public AuthorMismatchException(String message) {
+        super(message);
+    }
+}
